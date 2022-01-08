d869c6a2ab9766c4bd1cb3b752fc7e130821a73e
baimao shih <baimao@xfers.com>
2022-01-08 15:25:07 +0800

    basic style

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -22,8 +22,8 @@ const Todos = (userId) => {
 
   if (!currentUser) return null
   return (
-    <div>
-      <h1>My Todo List</h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-2xl my-1 font-bold">My Todo List</h1>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
@@ -49,9 +49,20 @@ const Todos = (userId) => {
                 name="name"
                 render={({ input, meta }) => {
                   return (
-                    <div>
-                      <input {...input} placeholder="Name" />
-                      {meta.dirty && <button type="submit">Save</button>}
+                    <div className="my-1">
+                      <input
+                        className="border-2 px-2 mx-1 rounded-md border-l-violet-100"
+                        {...input}
+                        placeholder="Name"
+                      />
+                      {meta.dirty && (
+                        <button
+                          className=" bg-violet-500 hover:bg-violet-700 rounded-md px-1"
+                          type="submit"
+                        >
+                          Save
+                        </button>
+                      )}
                     </div>
                   )
                 }}
@@ -62,7 +73,6 @@ const Todos = (userId) => {
       </ul>
       <div>
         <Form
-          submitText="Add Todo"
           onSubmit={async (values) => {
             try {
               await createTodoMutation({ userId: currentUser?.id, ...values })
@@ -78,7 +88,23 @@ const Todos = (userId) => {
           <Field
             name="name"
             render={({ input, meta }) => {
-              return <input {...input} placeholder="New Todo" />
+              return (
+                <div>
+                  <input
+                    className="border-2 px-2 mx-1 rounded-md border-l-violet-100"
+                    {...input}
+                    placeholder="New Todo"
+                  />
+                  {meta.dirty && (
+                    <button
+                      className=" bg-violet-500 hover:bg-violet-700 rounded-md px-1"
+                      type="submit"
+                    >
+                      Add Todo
+                    </button>
+                  )}
+                </div>
+              )
             }}
           />
         </Form>
@@ -93,7 +119,10 @@ const UserInfo = () => {
 
   if (currentUser) {
     return (
-      <>
+      <div className="flex items-center justify-between">
+        <div>
+          User name: <code>{currentUser.name}</code>
+        </div>
         <button
           className="button small"
           onClick={async () => {
@@ -102,14 +131,7 @@ const UserInfo = () => {
         >
           Logout
         </button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-          <br />
-          User name: <code>{currentUser.name}</code>
-        </div>
-      </>
+      </div>
     )
   } else {
     return (
@@ -131,165 +153,24 @@ const UserInfo = () => {
 
 const Home: BlitzPage = () => {
   return (
-    <div className="container">
-      <main>
-        <div className="logo">
+    <div className="container min-h-screen bg-gray py-6 flex flex-col justify-center relative overflow-hidden">
+      <main className="rounded-lg max-w-md mx-auto relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5">
+        <div className="logo px-10">
           <Image src={logo} alt="blitzjs" />
         </div>
-        <div>
-          <Suspense fallback="Loading...">
-            <Todos />
-          </Suspense>
-        </div>
-        <div className="buttons" style={{ marginTop: "1rem", marginBottom: "1rem" }}>
-          <Suspense fallback="Loading...">
-            <UserInfo />
-          </Suspense>
+        <div className="divide-y divide-violet-600 my-2">
+          <div>
+            <Suspense fallback="Loading...">
+              <Todos />
+            </Suspense>
+          </div>
+          <div className="my-4 py-2">
+            <Suspense fallback="Loading...">
+              <UserInfo />
+            </Suspense>
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://blitzjs.com?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Blitz.js
-        </a>
-      </footer>
-
-      <style jsx global>{`
-        @import url("https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;700&display=swap");
-
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: "Libre Franklin", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-          box-sizing: border-box;
-        }
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main p {
-          font-size: 1.2rem;
-        }
-
-        p {
-          text-align: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 60px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #45009d;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer a {
-          color: #f4f4f4;
-          text-decoration: none;
-        }
-
-        .logo {
-          margin-bottom: 2rem;
-        }
-
-        .logo img {
-          width: 300px;
-        }
-
-        .buttons {
-          display: grid;
-          grid-auto-flow: column;
-          grid-gap: 0.5rem;
-        }
-        .button {
-          font-size: 1rem;
-          background-color: #6700eb;
-          padding: 1rem 2rem;
-          color: #f4f4f4;
-          text-align: center;
-        }
-
-        .button.small {
-          padding: 0.5rem 1rem;
-        }
-
-        .button:hover {
-          background-color: #45009d;
-        }
-
-        .button-outline {
-          border: 2px solid #6700eb;
-          padding: 1rem 2rem;
-          color: #6700eb;
-          text-align: center;
-        }
-
-        .button-outline:hover {
-          border-color: #45009d;
-          color: #45009d;
-        }
-
-        pre {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          text-align: center;
-        }
-        code {
-          font-size: 0.9rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-            Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
     </div>
   )
 }
